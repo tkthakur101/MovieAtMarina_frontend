36d560a27bdab319d25928bcfb9bd10c3de5006e
tkthakur101 <66427087+tkthakur101@users.noreply.github.com>
2023-11-03 20:34:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
 
 function App() {
+ const [movies, setMovies] = useState();
+  const items = [];
+ const getMovies = async () =>{
+
+     try
+     {
+       const response = await api.get("/movie");
+       console.log(response.data);
+       setMovies(response.data);
+
+
+           for (const [key, value] of Object.entries(response.data)) {
+             items.push(<li key={key}>{key}: {value}</li>);
+           }
+           console.log(items);
+     }
+     catch(err)
+     {
+       console.log(err);
+     }
+   }
+     useEffect(() => {
+       getMovies();
+     },[])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <ul>{items}</ul>
     </div>
   );
 }
-
 export default App;
